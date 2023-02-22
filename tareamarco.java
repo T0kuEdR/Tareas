b7222d8993efd688866112fa13e2482a4ce4fb90
T0kuEdR <124408831+T0kuEdR@users.noreply.github.com>
2023-02-22 16:06:28 -0600

    Create tareamarco.java

---
+++ tareamarco.java
@@ -0,0 +1,9 @@
+# Tareas
+Garcia Santiago Emiliano POO 2208 FESA
+
+public class tareamarco {
+    public static void main(String[] args) {
+        String marco = " ------\n|      |\n|      |\n|      |\n ------";
+        System.out.println(marco);
+    }
+}
