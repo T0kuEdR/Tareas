b159e1a7a921b55d217b56c711742d20500b4594
T0kuEdR <124408831+T0kuEdR@users.noreply.github.com>
2023-03-25 21:54:41 -0600

    Update Cafetera.java

--- Cafetera/src/Cafetera.java
+++ Cafetera/src/Cafetera.java
@@ -66,7 +66,7 @@ public class Cafetera {
                 System.out.println("Puede seguir sirviendo\n");
             }
             else {
-                System.out.println("Ya no suficientes recursos para otro\n");
+                System.out.println("Ya no hay suficientes recursos para otro\n");
             }
         }
         else {
@@ -83,7 +83,7 @@ public class Cafetera {
                 System.out.println("Puede seguir sirviendo\n");
             }
             else {
-                System.out.println("Ya no suficientes recursos para otro\n");
+                System.out.println("Ya no hay suficientes recursos para otro\n");
             }
         }
         else {
@@ -101,7 +101,7 @@ public class Cafetera {
                 System.out.println("Puede seguir sirviendo\n");
             }
             else {
-                System.out.println("Ya no suficientes recursos para otro\n");
+                System.out.println("Ya no hay suficientes recursos para otro\n");
             }
         }
         else{
