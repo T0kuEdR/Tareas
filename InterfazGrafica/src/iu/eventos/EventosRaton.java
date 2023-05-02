b264754ec6943999f6ce9a74751b6ae65390b80e
T0kuEdR <124408831+T0kuEdR@users.noreply.github.com>
2023-05-02 00:26:43 -0600

    Add files via upload

---
+++ InterfazGrafica/src/iu/eventos/EventosRaton.java
@@ -0,0 +1,32 @@
+package iu.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EventosRaton implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Se dio clic");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
